6b514ad928a18294f9f2fa3098d4ca9a8249c939
Sebastian Segura <segurasebastian7887@gmail.com>
2019-06-25 00:53:21 -0300

    Updates

---
+++ precache-manifest.93613258167b0fd0e1a60e139cc05216.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbcffd70586cc9e26600609107ae8d04",
+    "url": "cloud.fbcf.svg"
+  },
+  {
+    "revision": "06c27b5494f16be1d90e69f701b1c25d",
+    "url": "cloud.fbcf.svg.br"
+  },
+  {
+    "revision": "b98118f3a6275ecad23a8b23874469fd",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "c37be6e8bf10c06e2c46b8150c6141de",
+    "url": "fonts/Inter-SemiBold.woff2"
+  },
+  {
+    "revision": "a35c949732d82304f366d5ccc165a375",
+    "url": "fonts/spacerangertitle.ttf"
+  },
+  {
+    "revision": "9390ac378d698288f1f956494706aa53",
+    "url": "index.html"
+  },
+  {
+    "revision": "d706d036348d5df0fac0",
+    "url": "main.89b2.css"
+  },
+  {
+    "revision": "d706d036348d5df0fac0",
+    "url": "main.d706.js"
+  },
+  {
+    "revision": "25c5b6e1c0236ac8c234dab94623434e",
+    "url": "main.d706.js.br"
+  },
+  {
+    "revision": "24668edd8f1f9619e48e",
+    "url": "vendor.2466.js"
+  },
+  {
+    "revision": "7723ddaa6c30227d0be4f706b8494681",
+    "url": "vendor.2466.js.br"
+  }
+]);
